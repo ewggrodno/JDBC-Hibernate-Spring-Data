f1f447bd767ff2a4e63cac3ad95697fdd04a1e27
ewggrodno <ewggrodno@gmail.com>
2018-05-12 16:44:31 +0300

    Configured connection to the database H2 - myTest

---
+++ src/main/java/bl/Util.java
@@ -0,0 +1,28 @@
+package bl;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+
+    private static final String DB_DRIVER = "org.h2.Driver";
+    private static final String DB_URL = "jdbc:h2:~/H2 - myTest";
+    private static final String DB_USERNAME = "ewgen";
+    private static final String DB_PASSWORD = "Alinochka2010";
+
+    public Connection getConnection(){
+        Connection connection = null;
+        try {
+            Class.forName(DB_DRIVER);
+            connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+            System.out.printf("Connection: OK!");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.printf("Connection: ERROR!");
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
+}
