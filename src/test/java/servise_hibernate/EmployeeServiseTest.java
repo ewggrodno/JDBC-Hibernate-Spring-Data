73c370bac3dbe9d7ee6de4b4609d10ccdcdd4f9e
ewggrodno <ewggrodno@gmail.com>
2018-05-27 16:38:40 +0300

    Create clsses for testing servise hibernate

---
+++ src/test/java/servise_hibernate/EmployeeServiseTest.java
@@ -0,0 +1,28 @@
+package servise_hibernate;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EmployeeServiseTest {
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void getById() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void remove() {
+    }
+}
